9fd3e3b402be3bbb549f0c7c0f43fe70a60cadba
vladrrom <berdenevlad@gmail.com>
2026-04-07 16:06:54 +0400

    feat: добавлен base page

---
+++ pages/base_page.py
@@ -0,0 +1,12 @@
+class BasePage:
+    def __init__(self, driver):
+        self.driver = driver
+
+    def open(self, url):
+        self.driver.get(url)
+
+    def find(self, locator):
+        return self.driver.find_element(*locator)
+
+    def click(self, locator):
+        self.find(locator).click()
