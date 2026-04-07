a1e96a40e824ca6e5d4c32aa8e2d086c0681bceb
vladrrom <berdenevlad@gmail.com>
2026-04-07 20:36:10 +0400

    feat: добавлены негативные тесты логина

--- tests/test_login.py
+++ tests/test_login.py
@@ -10,3 +10,26 @@ class TestLogin:
         inventory = InventoryPage(driver)
         assert inventory.is_opened()
         
+        
+    def test_wrong_password(driver):
+        login = LoginPage(driver)
+        login.open()
+        login.login("standard_user", "123456")
+
+        assert login.get_error_message() == "Epic sadface: Username and password do not match any user in this service"
+        
+    
+    def test_locked_user(driver):
+        login = LoginPage(driver)
+        login.open()
+        login.login("locked_out_user", "secret_sauce")
+        
+        assert login.get_error_message() == "Epic sadface: Sorry, this user has been locked out."
+        
+        
+    def test_empty_fields(driver):
+        login = LoginPage(driver)
+        login.open()
+        login.click_login()
+        
+        assert login.get_error_message() == "Epic sadface: Username is required"
